6f0d652758ae25c3c0dd8d7de2fcf2fa2fa2f77b
Fenson-F <fdfenlon@gmail.com>
2023-02-27 20:52:45 -0500

    A4.2 Added Gitignore

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /home/fenson/aesd-assignments/assignment-4-Fenson-F/assignments-3-and-later-Fenson-F/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
