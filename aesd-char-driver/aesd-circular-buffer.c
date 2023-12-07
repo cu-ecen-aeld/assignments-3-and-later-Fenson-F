fe33f7f46c781833dc18f8f3bf6a8dd121ff9d42
Fenson-F <fdfenlon@gmail.com>
2023-12-07 17:45:00 -0500

    A7.1 Circular Buffer completed

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -26,12 +26,78 @@
  * @return the struct aesd_buffer_entry structure representing the position described by char_offset, or
  * NULL if this position is not available in the buffer (not enough data is written).
  */
+
+/**
+     * An array of pointers to memory allocated for the most recent write operations
+     */
+    //struct aesd_buffer_entry  entry[AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED];
+    /**
+     * The current location in the entry structure where the next write should
+     * be stored.
+     */
+    //uint8_t in_offs;
+    /**
+     * The first location in the entry structure to read from
+     */
+    //uint8_t out_offs;
+    /**
+     * set to true when the buffer entry structure is full
+     */
+    //bool full;
+
+    // max operations => AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED
+
+
+
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
-{
-    /**
-    * TODO: implement per description
-    */
+{   
+    int8_t index = buffer->out_offs;;
+    struct aesd_buffer_entry *currententry = NULL;
+    int8_t preventrysize = 0;
+    int8_t entrytot = 0;
+    int8_t maxentrysize = 0;
+    int8_t entryoffset = 0;
+
+    //check that pointers are setup properly
+    if(buffer == NULL || entry_offset_byte_rtn == NULL){
+        return NULL;
+    }
+
+
+    // whille loop used as the AESD_CIRCULAR_BUFFER_H kept failing
+    while((index != buffer->out_offs) || (entrytot == 0))
+    {   
+        //update all variables to current index entry before checking if we found the offset
+        currententry = &buffer->entry[index]; 
+        maxentrysize = preventrysize  + currententry->size;
+        entryoffset = char_offset - preventrysize ;
+
+        //want to make sure the offset is between the maximum possible size with the current entry 
+        // and greater then the size of all previous entries
+        if( (char_offset < maxentrysize) && (char_offset >= preventrysize ) ){
+            
+            *entry_offset_byte_rtn = entryoffset;
+            return currententry;
+  
+        }
+
+        // if not found, continue with while loop by increasing total size checked and index value
+        if (currententry->buffptr != NULL){
+            preventrysize  = preventrysize + currententry->size;
+        }
+        
+        index++;
+        if(index >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED) {
+            index = 0;
+        }
+        
+        //check that we have gone through at least one loop
+        entrytot++;
+
+    }
+   
+   // return null if we've gone through it all
     return NULL;
 }
 
@@ -44,9 +110,36 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    //check that pointers are setup properly
+    if(buffer == NULL || add_entry == NULL){
+        return;
+    }
+
+    //add entry
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
+    
+    //advance pointer, declaring new variable to aid in debugging
+    int8_t newsize = buffer->in_offs + 1;
+
+    if(newsize >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+        buffer->in_offs = 0;
+    }
+    else
+    {
+        buffer->in_offs++;
+    }
+    
+
+    //check if full and set if true plus reseting out_offs to the new start location specified by in_offs
+    if( (buffer->in_offs == buffer->out_offs) || (buffer->full == true) )
+    {
+        buffer->full = true;
+
+        buffer->out_offs = buffer->in_offs;
+    }
+
 }
 
 /**
